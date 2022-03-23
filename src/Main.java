9fe174f99b89b702c62b35a79aed1c155114dc53
herbert <herbert.rocha@dcx.ufpb.br>
2022-03-23 04:16:11 +0000

    Version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
-    public static void main(String args){
-        System.out.println("teste");
+    public static void main(String[] args){
+        int numtest = 2;
+        int cont = 500000; //ate qual numero ele ira testar
+        while(numtest < cont){
+            boolean primo = testprimo(numtest);
+            if(primo){
+                System.out.println(numtest);
+            }
+            numtest++;
+
+        }
+
+    }
+
+
+    public static boolean testprimo(int numtest) {
+
+        int tests = 3;
+        boolean result = true;
+        while (tests < numtest) {
+
+            if ((numtest % tests) == 0) {
+                result = false;
+                break;
+            } else {
+                tests++;
+            }
+
+        }
+        return result;
     }
 }
