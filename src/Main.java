92304de0ca4e8b0c719a09f8cbc040954c94da93
Gabriel Alexandre da Silva <91101412+GbrielAlex@users.noreply.github.com>
2022-03-24 00:24:36 -0300

    troca while pelo for

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,20 @@ public class Main {
 
     public static void main(String[] args){
         int numtest = 2;
-        int cont = 500000; //ate qual numero ele ira testar
-        while(numtest < cont){
+        for(int cont = 50000;numtest < cont;numtest++){
             boolean primo = testprimo(numtest);
             if(primo){
                 System.out.println(numtest);
             }
-            numtest++;
-
         }
-
     }
 
-
     public static boolean testprimo(int numtest) {
-
         int tests;
         for (tests = 2; tests*tests <= numtest; tests++){
-
             if ((numtest % tests) == 0) {
                 return false;
             }
-
         }
         return true;
     }
